fba1791e3cf23846ce6e23ff769bb8b58b822b1a
Ahmed Hisham <ahmedhishammohmed@gmail.com>
2023-09-18 23:52:17 +0200

    Firebase auth

--- app/src/main/java/com/example/roomchatapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/roomchatapp/ui/login/LoginActivity.kt
@@ -3,15 +3,42 @@ package com.example.roomchatapp.ui.login
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.roomchatapp.R
 import com.example.roomchatapp.databinding.ActivityLoginBinding
+import com.example.roomchatapp.ui.showDialog
 
 class LoginActivity : AppCompatActivity() {
     lateinit var viewBinding: ActivityLoginBinding
+    lateinit var viewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initViews()
+        subscribeToLiveData()
+    }
+
+    private fun subscribeToLiveData() {
+        viewModel.errorLiveData.observe(this) { message ->
+            showDialog(
+                message = message.message ?: "Something went wrong!",
+                posActionName = "OK",
+                posAction = { dialog, i ->
+                    dialog.dismiss()
+                }
+            )
+
+        }
+    }
+
+    private fun initViews() {
         viewBinding = DataBindingUtil
             .setContentView(this, R.layout.activity_login)
+
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+        viewBinding.lifecycleOwner = this
+        viewBinding.vm = viewModel
+
     }
 }
--- app/src/main/java/com/example/roomchatapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/roomchatapp/ui/login/LoginViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.roomchatapp.ui.register
+package com.example.roomchatapp.ui.login
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -6,74 +6,53 @@ import com.example.roomchatapp.ui.ViewError
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class RegisterViewModel : ViewModel() {
+class LoginViewModel : ViewModel() {
     val errorLiveData = MutableLiveData<ViewError>()
     val isLoading = MutableLiveData<Boolean>()
-    val name = MutableLiveData<String>("Ahmed Hisham")
+    
     val email = MutableLiveData<String>("a.hisham.desoky@gmail.com")
-    val password = MutableLiveData<String>()
-    val passwordConfirmation = MutableLiveData<String>()
+    val password = MutableLiveData<String>("")
 
-    val nameError = MutableLiveData<String>()
     val emailError = MutableLiveData<String>()
     val passwordError = MutableLiveData<String>()
-    val passwordConfirmError = MutableLiveData<String>()
 
-    //    val auth = FirebaseAuth.getInstance()
-    private val auth = Firebase.auth
+    val auth = Firebase.auth
+
 
     private fun validForm(): Boolean {
         var isValid = true
-
-        if (name.value.isNullOrBlank()) {
-            nameError.postValue("Enter your full name!")
-            isValid = false
-        } else {
-            nameError.postValue("")
-        }
-
         if (email.value.isNullOrBlank()) {
-            emailError.postValue("Enter your email!")
+            emailError.postValue("Enter Email")
             isValid = false
         } else {
             emailError.postValue("")
         }
 
         if (password.value.isNullOrBlank()) {
-            passwordError.postValue("Enter password!")
+            passwordError.postValue("Enter password")
             isValid = false
         } else {
             passwordError.postValue("")
         }
-
-        if (passwordConfirmation.value != password.value) {
-            passwordConfirmError.postValue("Password doesn't match")
-            isValid = false
-        } else {
-            passwordConfirmError.postValue("")
-        }
-
         return isValid
 
     }
 
-    fun register() {
+    fun login() {
         if (!validForm()) return
 
-        //show loading pb
         isLoading.postValue(true)
-        auth.createUserWithEmailAndPassword(email.value!!, password.value!!)
+        auth.signInWithEmailAndPassword(email.value!!, password.value!!)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    // show message is i
                     isLoading.postValue(false)
-                    // show message
                     errorLiveData.postValue(
                         ViewError(
                             message = task.result.user?.uid
                         )
                     )
                 } else {
-                    // show error message
                     isLoading.postValue(false)
                     errorLiveData.postValue(
                         ViewError(
@@ -81,8 +60,6 @@ class RegisterViewModel : ViewModel() {
                         )
                     )
                 }
-
             }
-
     }
 }
